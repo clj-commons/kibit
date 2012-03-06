2be975a918355dc5590137d45601e3b73e06d7ba
Paul deGrandis <paul.degrandis@gmail.com>
2012-03-06 14:18:13 -0500

    Control structures updated for new core changes

--- src/jonase/kibit/control_structures.clj
+++ src/jonase/kibit/control_structures.clj
@@ -1,10 +1,10 @@
 (ns jonase.kibit.control-structures)
 
 (def rules
-  '{(if ?x ?y nil) when
-    (if ?x nil ?y) when-not
-    (if ?x (do . ?y)) when
-    (if (not ?x) ?y ?z) if-not
-    (when (not ?x) . ?y) when-not
-    (if true ?x . ?y) "do or removing the if"})
+  '{(if ?x ?y nil) (when ?x ?y)
+    (if ?x nil ?y) (when-not ?x ?y)
+    (if ?x (do . ?y)) (when ?x ?y)
+    (if (not ?x) ?y ?z) (if-not ?x ?y ?z)
+    (when (not ?x) . ?y) (when-not ?x ?y)
+    (if true ?x . ?y) ?x})
 
