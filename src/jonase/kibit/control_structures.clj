3fb727bb44c733845fd575238f19460faa28e1ad
Jonas Enlund <jonas.enlund@gmail.com>
2012-03-07 22:10:46 +0200

    Fixed some control-structure rules

--- src/jonase/kibit/control_structures.clj
+++ src/jonase/kibit/control_structures.clj
@@ -4,10 +4,10 @@
   '{(if ?x ?y nil) (when ?x ?y)
     (if ?x nil ?y) (when-not ?x ?y)
     (if ?x (do . ?y)) (when ?x ?y)
-    (if (not ?x) ?y ?z) (if-not ?x ?y)
+    (if (not ?x) ?y ?z) (if-not ?x ?y ?z)
     (when (not ?x) . ?y) (when-not ?x ?y)
-    (if true ?x . ?y) ?x
-    (when true ?x) ?x}) ; Maybe this should be (do ?x)
+    (if true ?x ?y) ?x
+    (when true . ?x) (do . ?x)}) ; Maybe this should be (do ?x)
 
 (comment
   (when true
