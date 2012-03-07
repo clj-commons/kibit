2d0d9442b26ae50014557b9691375a31060fc037
Jonas Enlund <jonas.enlund@gmail.com>
2012-03-07 21:45:39 +0200

    Added fn rules

--- src/jonase/kibit/misc.clj
+++ src/jonase/kibit/misc.clj
@@ -9,8 +9,18 @@
 
     ;; filter
     (filter (complement ?pred) ?coll) (remove ?pred ?coll) 
-    (filter #(not (?pred ?x)) ?coll) (remove ?pred ?coll)})
+    (filter #(not (?pred ?x)) ?coll) (remove ?pred ?coll)
+
+    ;; fn -- TODO: doesn't work yet
+    (fn [. ?arg] (?fun . ?arg)) ?fun
+    (fn* [. ?args] (?fun . ?args)) ?fun
+
+    })
 
 (comment
   (apply concat (apply map f (apply str (interpose \, "Hello"))))
-  (filter (complement nil?) [1 2 3]))
+  (filter (complement nil?) [1 2 3])
+
+  (map (fn [x] (inc x)) [1 2 3])
+  (map #(dec %) [1 2 3]))
+
