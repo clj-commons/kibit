bebabfd493e8e7206400aa10a312b40fb24e0d8a
Jonas Enlund <jonas.enlund@gmail.com>
2012-03-07 21:27:55 +0200

    Fixed misc rules

--- src/jonase/kibit/misc.clj
+++ src/jonase/kibit/misc.clj
@@ -1,8 +1,8 @@
 (ns jonase.kibit.misc)
 
 (def rules
-  '{(apply str (interpose ?x ?y)) clojure.string/join
+  '{(apply str (interpose ?x ?y)) (clojure.string/join ?x ?y)
 
     ;; mapcat
-    (apply concat (apply map ?x ?y)) mapcat
-    (apply concat (map ?x . ?y)) mapcat})
+    (apply concat (apply map ?x ?y)) (mapcat ?x ?y)
+    (apply concat (map ?x . ?y)) (mapcat ?x . ?y)})
