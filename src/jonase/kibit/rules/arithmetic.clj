33a403fb5f467333fe041264bfca9346ed2f65f2
Paul deGrandis <paul.degrandis@gmail.com>
2012-03-07 15:53:01 -0500

    arithmetic rules now return unified forms

--- src/jonase/kibit/rules/arithmetic.clj
+++ src/jonase/kibit/rules/arithmetic.clj
@@ -1,20 +1,23 @@
 (ns jonase.kibit.rules.arithmetic)
 
 (def rules
-  '{(+ ?x 1) inc
-    (+ 1 ?x) inc
-    (- ?x 1) dec
+  '{(+ ?x 1) (inc ?x)
+    (+ 1 ?x) (inc ?x)
+    (- ?x 1) (dec ?x)
 
-    (= 0 ?x)  zero?
-    (= ?x 0)  zero?
-    (== 0 ?x) zero?
-    (== ?x 0) zero?
+    (= 0 ?x)  (zero? ?x)
+    (= ?x 0)  (zero? ?x)
+    (== 0 ?x) (zero? ?x)
+    (== ?x 0) (zero? ?x)
     
-    (< 0 ?x)  pos?
-    (> ?x 0)  pos?
-    (<= 1 ?x) pos?
+    (< 0 ?x)  (pos? ?x)
+    (> ?x 0)  (pos? ?x)
+    (<= 1 ?x) (pos? ?x)
 
-    (< ?x 0) neg?})
+    (< ?x 0) (neg? ?x)
+    
+    (= ?x ?x)  true
+    (== ?x ?x) true})
 
     
     
