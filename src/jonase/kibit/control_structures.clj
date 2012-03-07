f1ee11629a2f70886f9421f41bb1a0e58d29f90e
Paul deGrandis <paul.degrandis@gmail.com>
2012-03-07 11:45:37 -0500

    Alts are now full forms

--- src/jonase/kibit/control_structures.clj
+++ src/jonase/kibit/control_structures.clj
@@ -2,10 +2,10 @@
 
 (def rules
   '{(if ?x ?y nil) (when ?x ?y)
-    (if ?x nil ?y) when-not
-    (if ?x (do . ?y)) when
-    (if (not ?x) ?y ?z) if-not
-    (when (not ?x) . ?y) when-not
+    (if ?x nil ?y) (when-not ?x ?y)
+    (if ?x (do . ?y)) (when ?x ?y)
+    (if (not ?x) ?y ?z) (if-not ?x ?y)
+    (when (not ?x) . ?y) (when-not ?x ?y)
     (if true ?x . ?y) ?x
     (when true ?x) ?x}) ; Maybe this should be (do ?x)
 
