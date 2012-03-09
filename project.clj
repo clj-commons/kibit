a104cbb19297ae91b6f59bf2746d962f730be368
Paul deGrandis <paul.degrandis@gmail.com>
2012-03-09 17:42:25 -0500

    General project docs and a quick URL doc for Marg

--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
 (defproject jonase/kibit "0.0.2-SNAPSHOT"
   :description "There's a function for that!"
+  :url "https://github.com/jonase/kibit"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "Contact if any questions"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.logic "0.6.7"]
                  [org.clojure/tools.namespace "0.1.2"]]
