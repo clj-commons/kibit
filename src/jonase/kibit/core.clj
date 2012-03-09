ce34ad1295784c8c916fff7c534d4035dd5a7344
Jonas Enlund <jonas.enlund@gmail.com>
2012-03-09 07:20:12 +0200

    Revert "Revert "Simplified unify map creation""
    This reverts commit 2c928187711f294a893eb76758e706dacd6c270e.

--- src/jonase/kibit/core.clj
+++ src/jonase/kibit/core.clj
@@ -39,10 +39,9 @@
     (when alt
       {:expr expr
        :rule rule
-       :alt (if (or (string? alt)
-                    (not (sequential? alt)))
-              alt
-              (seq alt))
+       :alt (if (seq? alt)
+              (seq alt)
+              alt)
        :line (-> expr meta :line)})))
 
 ;; Loop over the rule set.
