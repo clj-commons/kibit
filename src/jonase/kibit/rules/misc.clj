cd9f3016b0a344d709ffcb8329972c04dd3dbc28
Paul deGrandis <paul.degrandis@gmail.com>
2012-03-09 14:34:26 -0500

    Jonas fixes this in another branch

--- src/jonase/kibit/rules/misc.clj
+++ src/jonase/kibit/rules/misc.clj
@@ -11,7 +11,6 @@
     (filter (complement ?pred) ?coll) (remove ?pred ?coll) 
     (filter #(not (?pred ?x)) ?coll) (remove ?pred ?coll)
 
-    ;; anonymous functions
     (fn ?args (?fun . ?args)) ?fun
     (fn* ?args (?fun . ?args)) ?fun
 
