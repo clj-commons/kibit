a88574a04b55ef084031163861c86fce193b9726
Jonas Enlund <jonas.enlund@gmail.com>
2012-03-10 14:20:54 +0200

    Fixed (when (not ..) ..) rule -- fixes #13

--- src/jonase/kibit/rules/control_structures.clj
+++ src/jonase/kibit/rules/control_structures.clj
@@ -5,7 +5,7 @@
     (if ?x nil ?y) (when-not ?x ?y)
     (if ?x (do . ?y)) (when ?x ?y)
     (if (not ?x) ?y ?z) (if-not ?x ?y ?z)
-    (when (not ?x) . ?y) (when-not ?x ?y)
+    (when (not ?x) . ?y) (when-not ?x . ?y)
     (if true ?x ?y) ?x
     (when true . ?x) (do . ?x)
     (do ?x) ?x
@@ -14,6 +14,8 @@
     (if-let ?binding ?expr nil) (when-let ?binding ?expr)})
 
 (comment
+  (when (not (pred? x y)) (f x y))
+  
   (when true
     (if (pred? x)
       (do
