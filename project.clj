520c289e7ff0f3e6b4a8d512a1c5ccc4e6f7cbea
Jonas Enlund <jonas.enlund@gmail.com>
2012-03-16 06:00:45 +0200

    Update core.logic to 0.6.8

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :distribution :repo
             :comments "Contact if any questions"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.logic "0.6.7"]
+                 [org.clojure/core.logic "0.6.8"]
                  [org.clojure/tools.namespace "0.1.2"]]
   :dev-dependencies [[lein-marginalia "0.7.0"]]
   :eval-in-leiningen true
