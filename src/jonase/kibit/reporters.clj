8e527c2f323180e6e8b585aadc551c74238ac2b7
Paul deGrandis <paul.degrandis@gmail.com>
2012-03-19 20:48:38 -0400

    A new collection of possible reporters

---
+++ src/jonase/kibit/reporters.clj
@@ -0,0 +1,27 @@
+(ns jonase.kibit.reporters
+  (:require [clojure.string :as string]
+            [clojure.pprint :as pp])
+  (:import [java.io StringWriter]))
+
+;; A hack to get the code indented. 
+(defn pprint-code [form]
+  (let [string-writer (StringWriter.)]
+    (pp/write form
+              :dispatch pp/code-dispatch
+              :stream string-writer
+              :pretty true)
+    (->> (str string-writer)
+         string/split-lines
+         (map #(str "  " %))
+         (string/join "\n")
+         println))) 
+
+(defn cli-reporter [check-map]
+  (let [{:keys [line expr alt]} check-map]
+    (do 
+      (printf "[%s] Consider:\n" line)
+      (pprint-code alt)
+      (println "instead of:")
+      (pprint-code expr)
+      (newline))))
+
