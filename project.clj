2e1f33379eb11938d7c9dcad55259e53085a643a
Jonas Enlund <jonas.enlund@gmail.com>
2012-04-03 17:46:00 +0300

    Bump version to 0.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jonase/kibit "0.0.3"
+(defproject jonase/kibit "0.0.4-SNAPSHOT"
   :description "There's a function for that!"
   :url "https://github.com/jonase/kibit"
   :license {:name "Eclipse Public License - v 1.0"
