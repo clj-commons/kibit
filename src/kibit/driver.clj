b2f6c8aa1084a793da88443c987aaa0ac96f5dae
Ryan McGowan <ryan@ryanmcg.com>
2013-02-06 19:22:35 -0500

    Allow the user to select which reporter to use.
    Add a dependency on tools.cli.
    
    Usage:
    
        lein kibit -r markdown

--- src/kibit/driver.clj
+++ src/kibit/driver.clj
@@ -2,17 +2,21 @@
   (:require [clojure.tools.namespace :refer [find-clojure-sources-in-dir]]
             [clojure.java.io :as io]
             [kibit.check :refer [check-file]]
-            [kibit.reporters :refer [cli-reporter]]))
+            [kibit.reporters :refer :all]
+            [clojure.tools.cli :refer [cli]]))
+
+(def cli-specs [["-r" "--reporter"
+                 "The reporter used when rendering suggestions"
+                 :default "text"]])
 
 (defn run [project & args]
-  (let [source-files (if (empty? args)
+  (let [[options file-args usage-text] (apply (partial cli args) cli-specs)
+        source-files (if (empty? file-args)
                        (mapcat #(-> % io/file find-clojure-sources-in-dir)
                                (or (:source-paths project) [(:source-path project)]))
-                       args)]
+                       file-args)]
     (doseq [file source-files]
-      (try (->> (check-file file)
-                (map cli-reporter)
-                doall)
+      (try (check-file file :reporter (name-to-reporter (:reporter options)))
            (catch Exception e
              (println "Check failed -- skipping rest of file")
              (println (.getMessage e)))))))
