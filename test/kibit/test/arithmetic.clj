5f442bc39313ab8bc767c960436f199231ac2eae
Linus Ericsson <oscarlinusericsson@gmail.com>
2013-09-11 18:15:09 +0200

    added java.lang.Math-functions pow, hypot, round, expm1 and trivial identities

--- test/kibit/test/arithmetic.clj
+++ test/kibit/test/arithmetic.clj
@@ -9,4 +9,28 @@
     '(inc num) '(+ 1 num)
     '(dec num) '(- num 1)
     '(* x y z) '(* x (* y z))
-    '(+ x y z) '(+ x (+ y z))))
+    '(+ x y z) '(+ x (+ y z))
+
+    ;;hypot
+    '(Math/hypot x y) '(Math/sqrt (+ (Math/pow x 2) (Math/pow y 2)))
+    '(Math/hypot x y) '(Math/sqrt (+ (* x x) (* y y)))
+
+    ;;special exponential form
+    '(Math/expm1 x) '(- (Math/exp x) 1)
+    '(Math/expm1 x) '(dec (Math/exp x))
+
+    ;;not that ugly rounding trick, thank you
+    '(Math/round x) '(long (+ x 0.5))
+
+    ;;trivial identities
+    'x '(+ x 0) 
+    'x '(- x 0)
+    'x '(* x 1) 
+    'x '(/ x 1) 
+    '0 '(* x 0)
+    
+    ;;pow
+    '(Math/pow x 5) '(* x x x x x) 
+    '(Math/pow x 4) '(* x x x x) 
+    '(Math/pow x 3) '(* x x x) 
+    '(Math/pow x 2) '(* x x)))
