ac07e8d4f4f23760f5046479da21f7415afd69ef
Martin Clausen <martin.clausen@gmail.com>
2014-07-22 23:15:21 +0200

    Update collections.clj
    Added rules for nested calls to assoc to recommend assoc-in, instead of the more generic update-in.

--- src/kibit/rules/collections.clj
+++ src/kibit/rules/collections.clj
@@ -5,6 +5,9 @@
   ;;vector
   [(conj [] . ?x) (vector . ?x)]
   [(into [] ?coll) (vec ?coll)]
+  [(assoc ?coll ?key (assoc (?key0 ?coll) ?key1 ?val)) (assoc-in ?coll [?key0 ?key1] ?val)]
+  [(assoc ?coll ?key (assoc (?coll ?key0) ?key1 ?val)) (assoc-in ?coll [?key0 ?key1] ?val)]
+  [(assoc ?coll ?key (assoc (get ?coll ?key0) ?key1 ?val)) (assoc-in ?coll [?key0 ?key1] ?val)]
   [(assoc ?coll ?key (?fn (?key ?coll) . ?args)) (update-in ?coll [?key] ?fn . ?args)]
   [(assoc ?coll ?key (?fn (?coll ?key) . ?args)) (update-in ?coll [?key] ?fn . ?args)]
   [(assoc ?coll ?key (?fn (get ?coll ?key) . ?args)) (update-in ?coll [?key] ?fn . ?args)]
