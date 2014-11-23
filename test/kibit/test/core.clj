2220f694f12cbe245991418a937f4c3e9b019184
Daniel Compton <desk@danielcompton.net>
2014-11-24 12:05:50 +1300

    Upgrade core.logic dependency to 0.8.9
    Also upgrade tools.cli, and lein-marginalia

--- test/kibit/test/core.clj
+++ test/kibit/test/core.clj
@@ -1,11 +1,11 @@
 (ns kibit.test.core
   (:require [kibit.check :as kibit]
             [kibit.core :as core]
-            [clojure.core.logic :as logic]
+            [clojure.core.logic.unifier :as unifier]
             [kibit.rules :as core-rules])
   (:use [clojure.test]))
 
-(def all-rules  (map logic/prep core-rules/all-rules))
+(def all-rules  (map unifier/prep core-rules/all-rules))
 
 (deftest simplify-alts
   (are [expected-alt test-expr]
