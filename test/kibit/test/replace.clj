b366ddf1c8a5fd0cef5a04c9b840895e75af631f
James Brennan <james@jamesbrennan.ca>
2016-04-24 16:44:09 -0700

    Add tests for replace-expr

--- test/kibit/test/replace.clj
+++ test/kibit/test/replace.clj
@@ -3,7 +3,30 @@
             [kibit.rules :as rules]
             [kibit.replace :as replace])
   (:use [clojure.test])
-  (:import java.io.File))
+  (:import java.io.File
+           java.io.StringWriter))
+
+(defmacro discard-output
+  "Like `with-out-str`, but discards was was written to *out*"
+  [& body]
+  `(binding [*out* (StringWriter.)]
+     ~@body))
+
+(deftest replace-expr-are
+  (are [expected-form test-form]
+      (= expected-form
+         (discard-output
+          (replace/replace-expr test-form)))
+
+    '(inc a)
+    '(+ 1 a)
+
+    '(defn "Documentation" ^{:my-meta 1} [a]
+       ;; a comment
+       (inc a))
+    '(defn "Documentation" ^{:my-meta 1} [a]
+       ;; a comment
+       (+ 1 a))))
 
 (deftest replace-file-are
   (are [expected-form test-form]
@@ -11,7 +34,7 @@
          (let [file (doto (File/createTempFile "replace-file" ".clj")
                       (.deleteOnExit)
                       (spit test-form))]
-           (with-out-str (replace/replace-file file))
+           (discard-output (replace/replace-file file))
            (slurp file)))
 
     "(inc a)"
