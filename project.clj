b55204f4d3b349445dbc733076f282776d4803ed
Daniel Compton <desk@danielcompton.net>
2016-11-21 11:41:18 +1300

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jonase/kibit "0.1.3"
+(defproject jonase/kibit "0.1.4-SNAPSHOT"
   :description "There's a function for that!"
   :url "https://github.com/jonase/kibit"
   :license {:name "Eclipse Public License - v 1.0"
