1aa0a3576ddcee549565449379348706d87e56cd
Daniel Compton <desk@danielcompton.net>
2017-05-06 10:14:59 +1200

    Add kibit dependency to lein-kibit project.clj

--- lein-kibit/project.clj
+++ lein-kibit/project.clj
@@ -4,6 +4,7 @@
   :resource-paths ["../kibit-common/resources"]
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/tools.namespace "0.2.11"]]
+  :dependencies [[org.clojure/tools.namespace "0.2.11"]
+                 [jonase/kibit ~(clojure.string/trim-newline (slurp "../kibit-common/resources/jonase/kibit/VERSION"))]]
   :deploy-repositories [["releases" :clojars]]
   :eval-in-leiningen true)
