e91babeb6155d255b439c838625b264fa2514926
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-05-08 20:17:29 -0700

    Really test #78 - show that we can source analyze the failing nested #{} form

--- kibit/test/resources/sets.clj
+++ kibit/test/resources/sets.clj
@@ -1,4 +1,12 @@
 (ns resources.sets)
 
+(def Z_4
+  "This just shows that #78"
+  #{#{}
+    #{#{}}
+    #{#{#{}}}
+    #{#{#{#{}}}}
+    #{#{#{#{#{}}}}}})
+
 (defn killit [coll]
   (not-any? #{"string1" "string2"} (map ffirst coll)))
