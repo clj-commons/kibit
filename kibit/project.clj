463c6682f54059432391df46ef05fbc088ce7e39
Dr. Christian Betz <christian.betz@data42.de>
2019-04-18 10:14:34 +0200

    Update to latest dependencies
    TravisCI has problems building on OpenJDK 11 (which I do not have on AdoptOpenJDK 11). Maybe the problems are gone after upgrading (which I tend to like anyhow), so check.

--- kibit/project.clj
+++ kibit/project.clj
@@ -11,12 +11,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "Contact if any questions"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/core.logic "0.8.11"]
-                 [org.clojure/tools.cli "0.3.5"]
-                 [rewrite-clj "0.4.12"]
-                 [org.clojure/tools.reader "1.0.2"]]
-  :profiles {:dev {:dependencies [[lein-marginalia "0.9.0"]]
+                 [org.clojure/tools.cli "0.4.2"]
+                 [rewrite-clj "0.6.1"]
+                 [org.clojure/tools.reader "1.3.2"]]
+  :profiles {:dev {:dependencies [[lein-marginalia "0.9.1"]]
                    :resource-paths ["test/resources"]}}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
