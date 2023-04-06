6cea26f25230189fd2ff3ab1fc68c542052124ca
Noah Bogart <noah.bogart@hey.com>
2023-04-05 21:00:15 -0400

    Fix lein-kibit/project resource reference

--- lein-kibit/project.clj
+++ lein-kibit/project.clj
@@ -1,11 +1,11 @@
-(defproject lein-kibit (clojure.string/trim-newline (slurp "resources/jonase/kibit/VERSION"))
+(defproject lein-kibit (clojure.string/trim-newline (slurp "../resources/jonase/kibit/VERSION"))
   :description "kibit lein plugin"
   :url "https://github.com/jonase/kibit"
   :resource-paths ["resources"]
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.namespace "1.4.4"]
-                 [jonase/kibit ~(clojure.string/trim-newline (slurp "resources/jonase/kibit/VERSION"))]]
+                 [jonase/kibit ~(clojure.string/trim-newline (slurp "../resources/jonase/kibit/VERSION"))]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :eval-in-leiningen true)
